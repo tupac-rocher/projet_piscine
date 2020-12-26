22401605460af2acab829589e39ccde61bd8e7d7
Victoria <arbaouivictoria@gmail.com>
2020-12-26 19:09:04 +0100

    page html + début réservation

--- routes/reserver.js
+++ routes/reserver.js
@@ -6,10 +6,9 @@ const router = express.Router()
 router.get('/', async function(req, res){
     const allStudents = await etudiant.find()
     const allProfs = await prof.find()
-
     console.log(allStudents)
     console.log(allProfs)
-    res.render('creeReservation')
+    res.render('etu_creat_reservation')
 })
 
 
