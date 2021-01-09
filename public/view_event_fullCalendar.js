4d16a06f6c21dd95f63852d8ad9032dae05b86d8
Tupac <tupac.rocher@etu.umontpellier.fr>
2021-01-10 00:32:12 +0100

    arrangement 5

---
+++ public/view_event_fullCalendar.js
@@ -0,0 +1,9 @@
+console.log(event)
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
